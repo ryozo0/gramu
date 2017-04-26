ba1b72b804a87196b431437a9f24efd669a523c4
岡田亮介 <tobiuokun@gmail.com>
2017-04-26 16:36:13 +0900

    管理画面２

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,10 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
-       if user.admin?
-         can :manage, :all
-       else
-         can :read, :all
-      end
+    if user && user.admin?
+       can :access, :rails_admin   # grant access to rails_admin
+       can :manage, :all           # allow superadmins to do anything
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
